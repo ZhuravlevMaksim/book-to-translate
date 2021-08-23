749105515ae1aa7d1c88b28c551807ba87edb65a
Zhuravlev Maksim <m.zhuravlev@corp.rtln.ru>
2021-08-23 15:59:28 +0500

    read book in ext

--- popup/fileupload.js
+++ popup/fileupload.js
@@ -1,5 +1,15 @@
 document.getElementById("input").addEventListener("change", function(e) {
-    console.log(this.files);
+
+    const book = ePub(this.files[0]);
+
+    book.loaded.spine.then((spine) => {
+        spine.each((item) => {
+            item.load(book.load.bind(book)).then(({textContent}) => {
+                console.log(textContent);
+            });
+        });
+    });
+
 }, false);
 
 

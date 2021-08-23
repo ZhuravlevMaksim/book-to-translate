c3bed41f2fd4732a54030bd905d5a3b540b5e129
Zhuravlev Maksim <m.zhuravlev@corp.rtln.ru>
2021-08-23 21:26:34 +0500

    update parse

--- popup/fileupload.js
+++ popup/fileupload.js
@@ -1,15 +1,44 @@
-document.getElementById("input").addEventListener("change", function(e) {
+const area = document.getElementById('area');
+
+document.getElementById("input").addEventListener("change", async function (e) {
 
     const book = ePub(this.files[0]);
 
-    book.loaded.spine.then((spine) => {
-        spine.each((item) => {
-            item.load(book.load.bind(book)).then(({textContent}) => {
-                console.log(textContent);
-            });
-        });
-    });
+    const splitBook = []
+    const spine = await book.loaded.spine;
+
+    for (let item of spine.spineItems) {
+        const content = await item.load(book.load.bind(book))
+        const splitText = content.textContent.split(/(?!.”)(?<!Mr|Mrs|[\.\.])\./g)
+        splitText.forEach((raw) => {
+            const text = raw.replaceAll('\n', ' ')
+            splitBook.push(text)
+        })
+    }
+
+    console.log(splitBook)
+
+    splitBook.forEach(e => {
+        area.appendChild(document.createTextNode(e))
+        area.appendChild(document.createElement('br'))
+    })
 
 }, false);
 
+clearAll()
+initialize()
 
+function initialize() {
+    var gettingAllStorageItems = browser.storage.local.get(null);
+    gettingAllStorageItems.then((results) => {
+        console.log(results)
+    }, e => console.error(e));
+}
+
+function store(id, body) {
+    browser.storage.local.set({[id]: body});
+}
+
+function clearAll() {
+    browser.storage.local.clear();
+}
--- popup/popup.js
+++ popup/popup.js
@@ -1,13 +1,12 @@
 function listenForClicks() {
     document.addEventListener("click", (e) => {
-        console.log(e)
+        browser.tabs.create({url: "/popup/fileupload.html"}).then(() => {
+            browser.tabs.executeScript({
+                code: `console.log('location:', window.location.href);`
+            });
+        });
     })
 }
 
 browser.tabs.executeScript({file: "/index.js"}).then(listenForClicks);
 
-browser.tabs.create({url: "/popup/fileupload.html"}).then(() => {
-    browser.tabs.executeScript({
-        code: `console.log('location:', window.location.href);`
-    });
-});

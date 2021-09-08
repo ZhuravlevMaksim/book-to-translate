a2a9d3cbc751101cb065ad35bb4f010b9d9d608f
Zhuravlev Maksim <m.zhuravlev@corp.rtln.ru>
2021-09-08 21:37:00 +0300

    update regex

--- fileupload/fileupload.js
+++ fileupload/fileupload.js
@@ -7,16 +7,23 @@ document.getElementById("input").addEventListener("change", async function (e) {
 
     for (let item of spine.spineItems) {
         const content = await item.load(book.load.bind(book))
-        const splitText = content.textContent.split(/(?!”)(?<!Mr|Mrs|[ ])\./g)
+        const splitText = content.textContent
+            .replaceAll('\n', '')
+            .split(/(?<!Mr|Mrs| )(?!’)\./g)
         splitText.forEach((raw) => {
-            const text = raw.replaceAll('\n', ' ')
+            const text = raw
+                // .replaceAll('\n', '')
+                // .replaceAll(/[‘’]/g, "'")
             if (text.length > 1) {
-                splitBook.push(text)
+                splitBook.push(text.trim() + '.')
             }
         })
     }
 
+    browser.storage.local.clear();
     browser.storage.local.set({[this.files[0].name]: splitBook});
+
+    alert('done')
 
 }, false);
 

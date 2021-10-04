70598a2f0ad77ae0d1add94f7b924153665371c2
Zhuravlev Maksim <maks723@yandex.ru>
2021-10-04 21:13:36 +0300

    fix: close tab after load update parse

--- fileupload/fileupload.js
+++ fileupload/fileupload.js
@@ -7,24 +7,21 @@ document.getElementById("input").addEventListener("change", async function (e) {
 
     for (let item of spine.spineItems) {
         const content = await item.load(book.load.bind(book))
-        const splitText = content.textContent
-            .replaceAll('\n', '')
-            .split(/(?<!Mr|Mrs| )(?!’)\./g)
-        splitText.forEach((raw) => {
-            const text = raw
-                // .replaceAll('\n', '')
-                // .replaceAll(/[‘’]/g, "'")
-            if (text.length > 1) {
-                splitBook.push(text.trim() + '.')
+
+        for (let result of content.innerHTML.matchAll(/<p[^>]*>(.*?|[\s\S]*?)<\/p>/g)) {
+            const p = result[1].replaceAll(/\r?\n|\r/g, ' ').replaceAll(/<.+?>/g, '')
+
+            if (p.length > 1) {
+                splitBook.push(p)
             }
-        })
+        }
     }
 
     browser.storage.local.clear();
-    browser.storage.local.set({[this.files[0].name]: splitBook});
-
-    browser.tabs.getCurrent(function(tab) {
-        browser.tabs.remove(tab.id, function() { });
+    browser.storage.local.set({[this.files[0].name]: splitBook}).then(e => {
+        browser.tabs.getCurrent(function(tab) {
+            browser.tabs.remove(tab.id, function() { });
+        });
     });
 
 }, false);
@@ -35,4 +32,18 @@ function get(id) {
 
 function clearAll() {
     browser.storage.local.clear();
+}
+
+function oldSplit(content, splitBook) {
+    const splitText = content.textContent
+        .replaceAll('\n', '')
+        .split(/(?<!Mr|Mrs| )(?!’)\./g)
+    splitText.forEach((raw) => {
+        const text = raw
+            // .replaceAll('\n', '')
+            // .replaceAll(/[‘’]/g, "'")
+        if (text.length > 1) {
+            splitBook.push(text.trim() + '.')
+        }
+    })
 }

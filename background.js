1b542a14c61dbf8c936deab3d211b867b572e88b
Zhuravlev Maksim <maks723@yandex.ru>
2021-10-05 19:18:58 +0300

    add menu

--- background.js
+++ background.js
@@ -1,11 +1,3 @@
-browser.browserAction.onClicked.addListener(e => {
-    browser.tabs.create({url: "/fileupload/fileupload.html"}).then(() => {
-        browser.tabs.executeScript({
-            code: `console.log('location:', window.location.href);`
-        });
-    });
-});
-
 browser.tabs.onActivated.addListener(e => {
     browser.tabs.get(e.tabId, (tabInfo) => {
         if (tabInfo.url.startsWith('https://translate.google.com')) {

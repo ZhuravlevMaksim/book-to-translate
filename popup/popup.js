c816f273621d50dd60e2df1467abc2c45d74f2e1
Zhuravlev Maksim <maks723@yandex.ru>
2021-10-06 18:56:55 +0300

    fix popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,24 +1,20 @@
-function listenForClicks() {
-    document.addEventListener("click", (e) => {
-        switch (e.target.id) {
-            case 'add_book': {
-                browser.tabs.create({url: "/fileupload/fileupload.html"}).then(() => {
-                    browser.tabs.executeScript({
-                        code: `console.log('location:', window.location.href);`
-                    });
+document.addEventListener("click", (e) => {
+    switch (e.target.id) {
+        case 'add_book': {
+            browser.tabs.create({url: "/fileupload/fileupload.html"}).then(() => {
+                browser.tabs.executeScript({
+                    code: `console.log('location:', window.location.href);`
                 });
-                break
-            }
-            case 'open_google': {
-                browser.tabs.create({url: "https://translate.google.com"})
-                break
-            }
-            case 'open_yandex': {
-                browser.tabs.create({url: "https://translate.yandex.uz"})
-                break
-            }
+            });
+            break
         }
-    })
-}
-
-browser.tabs.executeScript({file: "/index.js"}).then(listenForClicks);
+        case 'open_google': {
+            browser.tabs.create({url: "https://translate.google.com"})
+            break
+        }
+        case 'open_yandex': {
+            browser.tabs.create({url: "https://translate.yandex.uz"})
+            break
+        }
+    }
+})
